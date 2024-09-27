4e9b0cc533fe962fd2de0a0e109edb2e9a960718
freebob2b <freebob2b@gmail.com>
2024-09-27 19:19:58 +0900

    fix: Change alt

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ export default function VideoCard({ video, type }) {
 
     return (
         <li className={`flex ${type ? 'flex-col mb-[15px]' : 'mb-[5px]'}`}>
-            <img src={thumbnails.high.url} alt="thumbnail image" className={`${type ? 'w-full mb-[10px]' : 'mr-[10px] w-[132.5px] xs:w-[200px] md:w-[132.5px]'}`} />
+            <img src={thumbnails.high.url} alt={title} className={`${type ? 'w-full mb-[10px]' : 'mr-[10px] w-[132.5px] xs:w-[200px] md:w-[132.5px]'}`} />
             <div className={`text-white ${type ? 'w-full' : 'w-[50%] flex-1'}`}>
                 <p className={`mb-[5px] font-semibold text-ellipsis line-clamp-2 break-all text-sm ${type && 'w-full text-sm mb-[5px]'}`}>{he.decode(title)}</p>
                 <div className='text-zinc-300'>
